b5b6e202f3ad7dfe7cfff79debf24d0d14196ef7
HasanAli44 <mdhasanali8439@gmail.com>
2024-11-15 18:36:43 +0600

    testimonial and footer area design complete

--- js/Script.js
+++ js/Script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".swiperbrand", {
+var swiper = new Swiper(".swipercourses", {
   speed: 4000,
   spaceBetween: 30,
   loop: true,
@@ -17,10 +17,37 @@ var swiper = new Swiper(".swiperbrand", {
     },
     768: {
       slidesPerView: 2,
-      spaceBetween: 40,
+      spaceBetween: 30,
     },
     1024: {
       slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+var swiper = new Swiper(".swipertestimonial", {
+  speed: 4000,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 20,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 4,
       spaceBetween: 30,
     },
   },
